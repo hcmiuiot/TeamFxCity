350d381bb9b4d57d9cd5165009edd157284664d9
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-10-28 16:53:14 +0700

    Try with opencv

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.opencv.core.Core;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         System.out.println("Test JavaFX Non external libs");
+        System.load(System.getProperty("user.dir") + "\\" + Core.NATIVE_LIBRARY_NAME + ".dll");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
